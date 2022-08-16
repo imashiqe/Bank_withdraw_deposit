30c7b73865e8f314109d42f9dfa872821fa0da2b
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-16 18:13:18 +0600

    Balance track app

---
+++ js/login.js
@@ -0,0 +1,5 @@
+// step - 1 : add click event handler with  the  submit button 
+
+document.getElementById('btn-submit').addEventListener('click', function(){
+    console.log('Submit Button Clicked');
+})
