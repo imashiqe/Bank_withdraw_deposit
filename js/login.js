e28042f1b87ca92ce1c3c2a4768d84b8d96ca0f3
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-16 19:23:07 +0600

    added font awsome and border

--- js/login.js
+++ js/login.js
@@ -1,5 +1,22 @@
 // step - 1 : add click event handler with  the  submit button 
 
 document.getElementById('btn-submit').addEventListener('click', function(){
-    console.log('Submit Button Clicked');
-})
+    // step - 2: get the email address inside the email input field
+    // always remember to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // step-3: get password
+    //3.1: set id on the html element
+    //3.2: get the element
+    //3.3: get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    //  Danger: Do not verify  email  password  on the client side.
+    // step -4: verofy email and password and check whether valid user or not
+    if(email === 'admin@yoursite.com' && password === 'admin'){
+        console.log('Valid User')
+    }
+    else{
+        console.log('Invalid User')
+    }
+})   
